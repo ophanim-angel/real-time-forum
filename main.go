6a82d8a90f726c14cb527a41a6ad58c925ffde9b
redshadow <simonouri38@gmail.com>
2026-02-24 00:20:47 +0000

    server init

--- main.go
+++ main.go
@@ -1,38 +1,22 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-// 	"toolkit/backend/models"
-// )
+	"toolkit/backend/models"
+)
 
-// func main() {
-// 	dbPath := "./database/forum.db"
-// 	db, err := models.InitDB(dbPath)
-// 	if err != nil {
-// 		log.Fatalf(" Erreur f l-database: %v", err)
-// 	}
-// 	defer db.Close()
-// 	fmt.Println("Database initialisée mzyan f:", dbPath)
+func main() {
+	dbPath := "./database/forum.db"
+	db, err := models.InitDB(dbPath)
+	if err != nil {
+		log.Fatalf("Error initialization DB: %v", err)
+	}
+	defer db.Close()
+	fmt.Println("Database ready and Tables created!")
 
-// 	if _, err := os.Stat("./frontend/index.html"); os.IsNotExist(err) {
-// 		fmt.Println("Warning: index.html makhdamch f path ./frontend/index.html")
-// 	} else {
-// 		fmt.Println("Frontend file (index.html) kayn.")
-// 	}
-
-// 	fs := http.FileServer(http.Dir("./frontend"))
-// 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/static"))))
-// 	http.Handle("/", fs)
-
-// 	port := ":8080"
-// 	fmt.Printf("Server khdam f http://localhost%s\n", port)
-
-// 	err = http.ListenAndServe(port, nil)
-// 	if err != nil {
-// 		log.Fatalf("Server mabhghach y-khdem: %v", err)
-// 	}
-// }
+	fmt.Println("Server starting at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
