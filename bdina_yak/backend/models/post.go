c5fae64a08df7629f66101e9e6919b3986173acf
redshadow <simonouri38@gmail.com>
2026-03-03 03:35:21 +0000

    adding post struct

---
+++ bdina_yak/backend/models/post.go
@@ -0,0 +1,29 @@
+package models
+
+// Post represents a post in the database
+type post struct {
+	ID        string `json:"id"`
+	UserID    string `json:"user_id"`
+	Nickname  string `json:"nickname"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	Category  string `json:"category"`
+	Views     string `json:"views"`
+	CreatedAt string `json:"created_at"`
+}
+
+// CreatePostInput: Data coming from frontend to create a post
+type CreatePostInput struct {
+	Title    string `json:"title"`
+	Content  string `json:"content"`
+	Category string `json:"category"`
+}
+
+// PostReaction represents a reaction (like, love, etc.)
+type PostReaction struct {
+	ID        string `json:"id"`
+	UderID    string `json:"user_id"`
+	PostID    string `json:"post_id"`
+	Type      string `json:"type"`
+	CreatedAt string `json:"created_at"`
+}
